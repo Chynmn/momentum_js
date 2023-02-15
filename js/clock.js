03bcb5405e6db7f8d0fb08807aa0e3a812ac0285
chynmn <chynmn0523@gmail.com>
2023-02-16 00:01:23 +0900

    [feat] Add real time

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,13 @@
 const clock = document.querySelector("#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-setInterval(sayHello, 5000);
+getClock();
+setInterval(getClock, 1000); // 지정한 시간 마다 함수를 실행함.
+// setTimeout은 지정한 시간에 함수를 실행함.
