2783aa9275e69f577590c33dd270161c708a6c92
chynmn <chynmn0523@gmail.com>
2023-02-16 20:40:23 +0900

    [chore] Create quotes

---
+++ js/quotes.js
@@ -0,0 +1,63 @@
+const quotes = [
+  {
+    quote: "자신의 데이터 구조를 파악하면 코드는 자연히 따라갈 것이다.",
+    author: "Programmer's proverb",
+  },
+  {
+    quote:
+      "좋은 소프트웨어의 기능이란 복잡한 것을 간단하게 보이도록 만드는 것이다.",
+    author: "Grady Booch",
+  },
+  {
+    quote: "지연되는 프로젝트에 인력을 더 투입하면 오히려 더 늦어진다.",
+    author: "Frederick Philips Brooks - Mythical Man-Month 저자",
+  },
+  {
+    quote:
+      "'낙관론'은 프로그래머라는 직업 관점에서 위험물이다.'피드백'이 해결책이다.",
+    author: "Kent Beck, Extreme - 프로그래밍 방법론의 창시자",
+  },
+  {
+    quote:
+      "에러 없는 프로그램을 만드는 데는 두가지 방법이 있다. 그런데 세번째 것만 작동한다.",
+    author: "Alan J. Perlis - 알골 프로그램 창시자",
+  },
+  {
+    quote: "스파게티 코드 소스가 주석 안달린 소스 나무란다.",
+    author: "Programmer's proverb",
+  },
+  {
+    quote:
+      "프로그래밍은 무엇을 알고 있는가에 대한 것이 아니다. 그것은 당신이 무엇을 알아낼 수 있는 가에 대한 것이다.",
+    author: "Chris Pine - Learn to program 의 저자",
+  },
+  {
+    quote:
+      "훌륭한 개발팀 없이 훌륭한 소프트웨어를 만들 수는 없다.그러나 대부분의 개발팀은 문제 있는 가족들처럼 행동한다.",
+    author: "Jim McCarthy - AI 용어를 만든사람, 인지 과학자",
+  },
+  {
+    quote:
+      "많은 프로젝트의 성공과 실패는 어떻게 수행하는가보다 누가 수행하는가에 따라 결정된다.",
+    author: "Robert L. Glass - 유명한 미국 소프트웨어 엔지니어 및 작가",
+  },
+  {
+    quote:
+      "바보는 복잡한 것을 무시하고, 현실주의자는 괴로워하며, 피하기도 한다. 하지만 천재는 복잡한 것을 없애버린다.",
+    author: "Alan J. Perlis - 알골 프로그램 창시자",
+  },
+  {
+    quote: "버그보고 놀란가슴 오타보고 놀란다.",
+    author: "Programmer's proverb",
+  },
+  {
+    quote:
+      "버그 리포트를 받으면 먼저 그 버그를 밖으로 드러나게 할 수 잇는 단위테스트를 작성하라.",
+    author: "Programmer's proverb",
+  },
+  {
+    quote:
+      "게으름이 자신의 가장 좋은 친구이다. 한 번에 자동화할 수 있는 것을 두 번 하지 마라.",
+    author: "Programmer's proverb",
+  },
+];
