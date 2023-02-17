b25a0a20d47aebcb8cddd6e000bee807da17e630
chynmn <chynmn0523@gmail.com>
2023-02-17 23:25:39 +0900

    [feat] add quote view

--- js/quotes.js
+++ js/quotes.js
@@ -61,3 +61,11 @@ const quotes = [
     author: "Programmer's proverb",
   },
 ];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
