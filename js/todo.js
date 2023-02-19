20738d06938756683927e00bfbd79b7e8a76b717
chynmn <chynmn0523@gmail.com>
2023-02-20 02:12:17 +0900

    [feat] Loading Todo

--- js/todo.js
+++ js/todo.js
@@ -3,6 +3,16 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+let toDos = []; // 기존의 todo array의 index들을 복원하기 위해 변수로 선언
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+  // JSON.stringify 는 JS코드를 string 형태로 만든다.
+  // JSON.parse 는 JS 객체로 재변환.
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -24,7 +34,17 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
