b7329586df9248ddad1709eec6e27265aadeb254
chynmn <chynmn0523@gmail.com>
2023-02-20 22:16:58 +0900

    [refator] background

--- js/background.js
+++ js/background.js
@@ -1,28 +1,96 @@
-const images = [
-  "0.jpg",
-  "1.jpg",
-  "2.jpg",
-  "3.jpg",
-  "4.jpg",
-  "5.jpg",
-  "5.jpg",
-  "6.jpg",
-  "7.jpg",
-  "8.jpg",
-  "9.jpg",
-  "10.jpg",
-  "11.jpg",
-  "12.jpg",
-  "13.jpg",
-  "14.jpg",
-  "15.jpg",
-  "16.jpg",
-];
+// const images = [
+//   "0.jpg",
+//   "1.jpg",
+//   "2.jpg",
+//   "3.jpg",
+//   "4.jpg",
+//   "5.jpg",
+//   "5.jpg",
+//   "6.jpg",
+//   "7.jpg",
+//   "8.jpg",
+//   "9.jpg",
+//   "10.jpg",
+//   "11.jpg",
+//   "12.jpg",
+//   "13.jpg",
+//   "14.jpg",
+//   "15.jpg",
+//   "16.jpg",
+// ];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+// const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+// const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+// bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
+
+const UNSPLASH_API_KEY =
+  "b491e86a6957b396f44f1e15e41d3d242e17aa982607f161b95defd195c7f4dd";
+const UNSPLASH_URL = `https://api.unsplash.com/photos/random/?client_id=${UNSPLASH_API_KEY}&query=landscape&orientation=landscape`;
+const body = document.querySelector("body"),
+  locationContainer = document.querySelector(".js-location span");
+
+function loadBackground() {
+  const savedImage = localStorage.getItem("bg");
+  if (savedImage === null) {
+    getBackground();
+  } else {
+    const parsedImage = JSON.parse(savedImage);
+    const today = new Date();
+    if (today > parsedImage.expiresOn) {
+      getBackground();
+    } else {
+      body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)), url(${parsedImage.url})`;
+      locationContainer.innerHTML = `${parsedImage.name}, ${parsedImage.city}, ${parsedImage.country}`;
+    }
+  }
+  return;
+}
+
+function saveBackground(imageUrl, city, country, name) {
+  const savedImage = localStorage.getItem("bg");
+  if (savedImage !== null) {
+    localStorage.removeItem("bg");
+  }
+  const expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + 1);
+  const imageObject = {
+    url: imageUrl,
+    expiresOn: expirationDate,
+    city,
+    country,
+    name,
+  };
+  localStorage.setItem("bg", JSON.stringify(imageObject));
+  loadBackground();
+  return;
+}
+
+function getBackground() {
+  fetch(UNSPLASH_URL)
+    .then((response) => response.json())
+    .then((json) => {
+      const image = json;
+      if (image.urls && image.urls.full && image.location) {
+        const fullUrl = image.urls.full;
+        const location = image.location;
+        const city = location.city;
+        const country = location.country;
+        const name = location.name;
+        saveBackground(fullUrl, city, country, name);
+      } else {
+        getBackground();
+      }
+    });
+  return;
+}
+
+function initApp() {
+  loadBackground();
+  return;
+}
+
+initApp();
